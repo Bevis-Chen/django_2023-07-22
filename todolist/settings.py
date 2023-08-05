df7dd9dc39e9f71158912d9980b878f92a1d84a2
Austin <Austin@gmail.com>
2023-08-05 11:59:44 +0800

    2nd upload_2023-08-05

--- todolist/settings.py
+++ todolist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-30**#7!at1emhg#yzmq3q((vq&3xvhzl6p2u@!=@$fhu62yh$8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "todo-django-uof2.onrender.com"]
 
 
 # Application definition
